6f2ed6d6d0f8f79c004a041dcc4a5c1974feabc2
Joel Alvarez <eynar.j.alvarez.m@gmail.com>
2024-06-12 23:58:28 -0400

    feat: define src basic folder structure, implement shop layout and components and define initial router routes.

---
+++ src/modules/shop/layouts/ShopLayout.vue
@@ -0,0 +1,20 @@
+<template>
+    <!-- component -->
+    <!-- Create By Joker Banny -->
+
+    <!-- Header Navbar -->
+    <top-menu />
+
+    <!-- RouterView -->
+    <RouterView />
+
+    <!-- Footer -->
+    <custom-footer />
+</template>
+
+<script setup lang="ts">
+import CustomFooter from '../components/CustomFooter.vue';
+import TopMenu from '../components/TopMenu.vue';
+
+
+</script>
